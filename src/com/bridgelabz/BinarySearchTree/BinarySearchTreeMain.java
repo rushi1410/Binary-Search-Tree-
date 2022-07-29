ecaf8c4113354f83cd12791c3cdbece6af6df553
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-29 17:21:08 +0530

    Added: Welcome To Binary Search Tree Program

---
+++ src/com/bridgelabz/BinarySearchTree/BinarySearchTreeMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.BinarySearchTree;
+
+public class BinarySearchTreeMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Binary Search Tree Program");
+	}
+}
